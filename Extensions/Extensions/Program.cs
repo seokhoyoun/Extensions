2e5d419ccc33f768d5dd9e62b65bb29e27432519
DESKTOP-KLGCS5E\axlov <axlover@hotmail.com>
2020-10-26 17:53:01 +0100

    - Testcase commit

--- Extensions/Extensions/Program.cs
+++ Extensions/Extensions/Program.cs
@@ -30,8 +30,9 @@ namespace Extensions
 
 
             byte[] testByteArr = new byte[] { 0xBB, 0xAA, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
             byte[] changedByteArr = testByteArr.SetIntToByteArr(8, 16, -444, EByteOrder.Motorola);
+            // 0xfe 0x44 0xff 0xff 0xff 0xff 0xff 0xff
+
             Debug.Assert( changedByteArr.GetIntFromByteArr(8, 16, EValueType.SignedValue, EByteOrder.Motorola) == -444);
 
             changedByteArr = changedByteArr.SetIntToByteArr(23, 16, -444, EByteOrder.Motorola);
@@ -41,11 +42,11 @@ namespace Extensions
             //nt testNum = 0x1FFFFFFF;
 
             //testNum.GetBitChangedIntegerByEndian(56, 12, 0, true);
-            byte[] floatByte = new byte[] { 0xc2, 0xed, 0x40, 0x00 };
             //floatByte = new byte[] { 0x00, 0x40, 0xED, 0xC2 };
 
-            //float a = -118.625f;
-            float b = floatByte.GetFloatFromByteArr(24, EByteOrder.Motorola);
+            //-118.625f;
+            byte[] floatByte = new byte[] { 0xc2, 0xed, 0x40, 0x00 };
+            Debug.Assert(floatByte.GetFloatFromByteArr(24, EByteOrder.Motorola) == -118.625f);
 
             floatByte = new byte[] { 0x49, 0x74, 0x23, 0xf0 };
             //0x33d6bf95
